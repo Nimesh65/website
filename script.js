04d40e5cb4d402f69217ff613592245413ac4d35
Nimesh65 <nimesh@toptechgaints.com>
2026-01-05 12:39:36 +0545

    navbar fixed

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const btn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-links-mobile');
-btn.addEventListener('click', ()=>{
+
+btn.addEventListener('click', () => {
   console.log("clicked");
   nav.classList.toggle("active");
+});
+
+// 🔽 HIDE MENU WHEN PAGE SCROLLS
+window.addEventListener('scroll', () => {
+  if (nav.classList.contains('active')) {
+    nav.classList.remove('active');
+  }
 });
